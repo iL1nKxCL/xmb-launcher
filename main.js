5af2d912a65b69c2b5f065040071f34862df994e
Lian Venegas Perez <lian-2010@hotmail.es>
2025-02-22 17:15:40 -0300

    Añadir configuraciones de pantalla completa y ventana

--- main.js
+++ main.js
@@ -1,33 +1,51 @@
-const { app, BrowserWindow, Menu } = require("electron");
-const { autoUpdater } = require("electron-updater");
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
-let mainWindow;
+let win;
 
-app.whenReady().then(() => {
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    fullscreen: false,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false, // Asegúrate de que esto esté configurado correctamente
+            enableRemoteModule: true,
+        }
+    });
 
-  mainWindow.loadFile("src/index.html");
+    win.loadFile(path.join(__dirname, 'index.html'));
+}
 
-  // Auto-Updater
-  autoUpdater.checkForUpdatesAndNotify();
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-autoUpdater.on("update-available", () => {
-  console.log("Nueva actualización disponible.");
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
 
-autoUpdater.on("update-downloaded", () => {
-  console.log("Actualización lista. Reiniciando...");
-  autoUpdater.quitAndInstall();
+// Manejar eventos de configuración
+ipcMain.on('toggle-fullscreen', () => {
+    if (!win.isFullScreen()) {
+        win.setFullScreen(true);
+    } else {
+        win.setFullScreen(false);
+    }
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+ipcMain.on('toggle-windowed', () => {
+    if (win.isFullScreen()) {
+        win.setFullScreen(false);
+        win.setSize(800, 600); // Tamaño de ventana en modo ventana
+    } else {
+        win.center();
+        alert('Modo ventana ya activado');
+    }
 });
